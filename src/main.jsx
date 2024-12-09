5c007d72e286d61d7b621c03589abb163a76568e
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-09 17:08:20 +0300

    Add React Router for navigation and remove unused App component and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import Home from './Home.jsx'
+import Contact from './Contact.jsx'
 
+//React routing for page pathes by assigning their routes and which elements it should render
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Home/>,
+  },
+  {
+    path: '/contact',
+    element:<Contact/>,
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
