14f2ae42ff5c217817b7ced4f04e66670c75c78e
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-11 23:44:35 +0300

    Add Footer component and integrate it into Home layout
    Finalized Home page UI Frontend

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Categories from "./components/Categories";
 import MostSearched from "./components/MostSearched";
+import Footer from "./components/Footer";
 
 function Home() {
     return (
@@ -15,6 +16,8 @@ function Home() {
             <Categories/>
             {/* Most Searched */}
             <MostSearched/>
+            {/* Footer */}
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-gray-50 dark:bg-gray-900">
+                <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                    <div className="sm:flex sm:items-center sm:justify-between">
+                        <div className="flex justify-center sm:justify-start gap-6 items-center">
+                            <img src="/logo.png" width={50} height={10} />
+                            <h2 className='font-semibold text-xl'>WheelDeal</h2>
+                        </div>
+
+                        <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right dark:text-gray-400">
+                            Copyright &copy; 2025. All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
