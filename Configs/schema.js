892917e3cb9bf1e4745aa1766e3de0c6115c44a5
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-15 17:37:44 +0300

    Add database schema for car listings and update neon configuration; include new drizzle commands in package.json

---
+++ Configs/schema.js
@@ -0,0 +1,26 @@
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+
+export const carListing = pgTable("carListing", {
+    id : serial('id').primaryKey(),
+    listingTitle : varchar('listingTitle').notNull(),
+    tagLine : varchar('tagLine'),
+    originalPrice : varchar('originalPrice'),
+    sellingPrice : varchar('sellingPrice').notNull(),
+    category : varchar('category').notNull(),
+    condition : varchar('condition').notNull(),
+    type : varchar('type').notNull(),
+    make : varchar('make').notNull(),
+    model : varchar('model').notNull(),
+    year : varchar('year').notNull(),
+    driveType : varchar('driveType').notNull(),
+    transmission : varchar('transmission').notNull(),
+    fuelType : varchar('fuelType').notNull(),
+    mileage : varchar('mileage').notNull(),
+    engineSize : varchar('engineSize'),
+    cylinder : varchar('cylinder'),
+    color : varchar('color').notNull(),
+    door : varchar('door').notNull(),
+    offerType : varchar('offerType'),
+    vin : varchar('vin'),
+    listingDescription : varchar('listingDescription').notNull(),
+});
