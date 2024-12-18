160a4ac9125c2981bafb6dae0ab461d6ac680af4
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-18 22:47:33 +0300

    Implement user-specific car listing retrieval and add result formatting service

--- src/Profile/Components/MyListing.jsx
+++ src/Profile/Components/MyListing.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { MdOutlineNoteAdd } from "react-icons/md";
 import { Button } from '@/components/ui/button'
+import { CarImages, carListing } from '../../../Configs/schema';
+import { eq, desc } from 'drizzle-orm';
+import { useUser } from '@clerk/clerk-react';
+import { db } from '../../../Configs/neon';
+import Service from '../../components/Shared/Service';
 
 function MyListing() {
+
+    const { user } = useUser();
+    
+    useEffect(() => {
+        user && getUserCarListing()
+    }, [user])
+
+    const getUserCarListing=async()=>{
+        const result = await db.select().from(carListing)
+        .leftJoin(CarImages, eq(carListing.id, CarImages.carListingId))
+        .where(eq(carListing.createdBy, user?.primaryEmailAddress?.emailAddress))
+        .orderBy(desc(carListing.id))
+
+        const resp = Service.FormatResult(result);
+        console.log(resp);
+    }
+
     return (
         <div className='mt-6'>
             <div className='flex justify-between items-center'>
