69cdcde6f53de9f8cf6ddad8bde6c21b1abcd22f
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-23 20:41:22 +0300

    Enhance OwnersDetail component in ListingDetail with dealer information and a message button

--- src/Listing/ListingDetails/Components/OwnersDetail.jsx
+++ src/Listing/ListingDetails/Components/OwnersDetail.jsx
@@ -1,9 +1,15 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function OwnersDetail({ carDetail }) {
     return (
-        <div className='p10 border rounded-xl shadow-md mt-7 '>
-            <img src={carDetail?.userImageUrl} className='w-[70px] h-[70px] rounded-full m-2' alt="" />
+        <div className='p-10 border rounded-xl shadow-md mt-'>
+            <h2 className='font-medium text-2xl mb-3'>Dealer Details</h2>
+            <img src={carDetail?.userImageUrl} className='w-[70px] h-[70px] rounded-full' alt="" />
+            <h2 className='mt-2 font-bold text-xl'>{carDetail?.userName}</h2>
+            <h2 className='mt-2 text-gray-500'>{carDetail?.createdBy}</h2>
+            
+            <Button className='w-full mt-6'>Message Owner</Button>
         </div>
     )
 }
