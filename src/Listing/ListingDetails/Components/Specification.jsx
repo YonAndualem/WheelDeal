860a7355d2c2d03cedd8c186fcd7bcebd5b84bfa
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-23 18:27:56 +0300

    Enhance Specification component in ListingDetail with improved layout and title for better readability

--- src/Listing/ListingDetails/Components/Specification.jsx
+++ src/Listing/ListingDetails/Components/Specification.jsx
@@ -5,11 +5,12 @@ import React from 'react'
 function Specification({carDetail}) {
     console.log(carDetail)
   return (
-    <div className='p-10 rounded-xl border shadow-md mt-7'>
+    <div className='p-10 rounded-xl border shadow-md mt-7 pt-4'>
+        <h2 className='font-medium text-2xl pb-3'>Specifications</h2>
         {CarSpecification.map((item, index)=>(
-            <div className='mt-5'>
-                
+            <div key={index} className='mt-5 flex items-center justify-between'>
                 <h2 className='flex gap-3'><IconField icon={item.icon} /> {item.label}</h2>
+                <h2>{carDetail ? carDetail[item.name] || 'N/A' : 'N/A'}</h2>
             </div>
         ))}
     </div>
