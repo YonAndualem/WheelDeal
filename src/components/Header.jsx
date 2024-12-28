076a5bee72410ac122d9b8e6ca1118736478c6d0
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-28 12:17:23 +0300

    Enhance Header component with user listings link and sign-in button; update styles for better visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { UserButton, useUser } from '@clerk/clerk-react'
+import { SignIn, SignInButton, UserButton, useUser } from '@clerk/clerk-react'
 import { Button } from './ui/button'
 import '@fontsource/montserrat';
 import { Link } from 'react-router-dom';
 import { MdOutlineNoteAdd } from "react-icons/md";
+import { FaListCheck } from "react-icons/fa6";
 
 function Header() {
 
     const { user, isSignedIn } = useUser();
     return (
-        <div className='flex justify-between items-center shadow-sm'>
+        <div className='flex justify-between items-center shadow-sm bg-slate-900 text-white'>
             <Link to="/">
                 <div className='justify-between flex items-center px-2'>
                     <img src="./logo.png" alt="logo" width={50} height={25} />
@@ -17,22 +18,25 @@ function Header() {
                 </div>
             </Link>
 
-            <ul className='hidden md:flex gap-16' style={{ fontFamily: 'Montserrat' }}>
-                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
-                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
-                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
-                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>PreOwned</li>
-            </ul>
+            
 
             {isSignedIn ?
-                <div className='flex items-center gap-5'>
-                    <UserButton />
-                    <Link to='/profile'>
-                        <Button className="mr-2">Submit Listing <MdOutlineNoteAdd /></Button>
-                    </Link>
+                <div className='flex items-center '>
+                    <div className='flex items-center gap-5'>
+                        <UserButton />
+                        <Link to='/listings'>
+                            <Button className="bg-slate-800">Your Listings <FaListCheck /></Button>
+                        </Link>
+                        <Link to='/profile'>
+                            <Button className="mr-2 bg-slate-800">Submit Listing <MdOutlineNoteAdd /></Button>
+                        </Link>
+                    </div>
+                    
                 </div>
                 :
-                <Button>Submit Listing</Button>
+                <SignInButton mode='modal'>
+                    <Button className="mr-3">Sign In</Button>
+                </SignInButton>
             }
 
         </div>
