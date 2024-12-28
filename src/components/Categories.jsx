3b7ac64a3de4cc4b934a9072ad38df65296f6899
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-28 14:06:08 +0300

    Enhance Categories component styling; update padding, background color, and hover effects for improved user experience

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,14 +3,14 @@ import Data from './Shared/Data'
 import { Link } from 'react-router-dom'
 function Categories() {
     return (
-        <div className='mt-40'>
-            <h2 className='font-bold text-3xl text-center mb-6'>Browse by type</h2>
+        <div className='py-8'>
+            <h2 className='font-bold text-3xl text-center mb-6'>Browse by Type</h2>
 
-            <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20">
+            <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20 ">
                 {Data.Categories.map((category, index) => (
-                    <Link to={'search/'+category.name}>
-                        <div key={category.id} className="border rounded-xl p-3 items-center flex flex-col hover:shadow-md cursor-pointer">
-                            <img src={category.icon} alt={category.name} width={60} height={60}/>
+                    <Link to={'search/'+category.name} key={category.id}>
+                        <div className="border rounded-xl p-3 items-center flex flex-col hover:shadow-2xl cursor-pointer bg-slate-900">
+                            <img src={category.icon} alt={category.name} width={60} height={60} className='text-white' style={{ filter: 'invert(5)' }}/>
                             <h2 className="mt-2">{category.name}</h2>
                         </div>
                     </Link>
