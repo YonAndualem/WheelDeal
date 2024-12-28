32ae7187fc2f774de697259f4f58bc13b0ab171e
Yonas B.Andualem <yonasandualem1472@gmail.com>
2024-12-28 17:04:29 +0300

    Update OwnersDetail.jsx

--- src/Listing/ListingDetails/Components/OwnersDetail.jsx
+++ src/Listing/ListingDetails/Components/OwnersDetail.jsx
@@ -8,7 +8,7 @@ function OwnersDetail({ carDetail }) {
             <img src={carDetail?.userImageUrl} className='w-[70px] h-[70px] rounded-full' alt="" />
             <h2 className='mt-2 font-bold text-xl'>{carDetail?.userName}</h2>
             <h2 className='mt-2 text-gray-500'>{carDetail?.createdBy}</h2>
-
+            
             <Button className='w-full mt-6 bg-slate-700'>Message Owner</Button>
         </div>
     )
